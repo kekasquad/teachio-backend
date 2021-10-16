69eb58349db624b9d4c9906657e4b8f9f0dcea79
Ivan Angelov <angelow.van@yandex.ru>
2021-10-16 23:24:02 +0300

    Added connection to database and data model initialization

--- core/main.py
+++ core/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 async def root():
     return {'message': 'Core Service'}
+
+
+register_tortoise(
+    app,
+    db_url='postgres://postgres:s3cret@localhost:5432/teachio',
+    modules={'models': ['models']},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
