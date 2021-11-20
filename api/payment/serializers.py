6203f7b8f6dc380249584a042205481f469a7881
Vladislav <vlad@grokhotov.ru>
2021-11-21 01:33:12 +0300

    add payment

---
+++ api/payment/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from payment.models import Payment
+
+
+class PaymentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        read_only_fields = ('id', 'status')
+        fields = read_only_fields + ('cost', 'created', 'updated')
+
+
+class PaymentRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        read_only_fields = ('id', 'status', 'cost', 'created', 'updated')
+        fields = read_only_fields
+
+
+class PaymentUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        read_only_fields = ('id', 'created', 'updated')
+        fields = read_only_fields + ('status', 'cost')
