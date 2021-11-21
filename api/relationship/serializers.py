39200460210c7975a8cadf70b998a95c80fc0bee
Vladislav <vlad@grokhotov.ru>
2021-11-21 21:48:34 +0300

    hotfix - add relationship validation

--- api/relationship/serializers.py
+++ api/relationship/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from relationship.models import Relationship
 
@@ -8,6 +9,14 @@ class RelationshipCreateSerializer(serializers.ModelSerializer):
         model = Relationship
         read_only_fields = ('created', 'updated')
         fields = read_only_fields + ('student', 'teacher')
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        if attrs.get('student').is_teacher:
+            raise ValidationError({'student': 'student is teacher'})
+        if not attrs.get('teacher').is_teacher:
+            raise ValidationError({'teacher': 'teacher is student'})
+        return attrs
 
 
 class RelationshipRetrieveSerializer(serializers.ModelSerializer):
