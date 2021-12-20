076a4c85a1ea6a74cf0ba098db9bdbff29431875
Danil Beltyukov <beltyukovdanil@gmail.com>
2021-12-20 03:18:42 +0300

    add logging

--- message/receivers.py
+++ message/receivers.py
@@ -1,3 +1,4 @@
+import logging
 from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -5,16 +6,19 @@ from django.dispatch import receiver
 from notification.fcm import send_notification
 from .models import Message
 
+logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=Message)
 def message_create_update_receiver(created, instance, **_):
+    logger.info(f"Post-save Message: ${created} ${instance!r}")
     if created:
-#         with transaction.atomic():
-#             if instance.sender.is_teacher:
-#                 instance.chat.student_unread += 1
-#             else:
-#                 instance.chat.teacher_unread += 1
-#             instance.chat.save()
+        #         with transaction.atomic():
+        #             if instance.sender.is_teacher:
+        #                 instance.chat.student_unread += 1
+        #             else:
+        #                 instance.chat.teacher_unread += 1
+        #             instance.chat.save()
 
         if instance.sender.is_teacher:
             send_notification(
